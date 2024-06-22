580d81212846d0c2b2fd003fdb6725d481ef2100
Sergey Andronov <sergey@andronov.me>
2024-06-22 11:05:07 +0200

    Checkout: simple test for new method

--- spec/lib/checkout/checkout_spec.rb
+++ spec/lib/checkout/checkout_spec.rb
@@ -1,4 +1,11 @@
 require "spec_helper"
 
 RSpec.describe Checkout do
+  describe "#new" do
+    subject { Checkout.new }
+
+    it "creates a new instance of Checkout" do
+      is_expected.to be_an_instance_of(Checkout)
+    end
+  end
 end
