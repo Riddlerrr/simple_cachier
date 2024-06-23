10aadc4278bec723cbf6f342e424a8077c7df66f
Sergey Andronov <sergey@andronov.me>
2024-06-23 15:43:26 +0200

    Offers: simplify apply_discount method

--- lib/offers.rb
+++ lib/offers.rb
@@ -15,10 +15,9 @@ class Offers
   end
 
   def apply_discount(item)
-    product_code = item.product_code.upcase.to_sym
-    return 0 unless @registered_offers.key?(product_code)
+    return 0 unless @registered_offers.key?(item.product_code)
 
-    @registered_offers[product_code].discount(item)
+    @registered_offers[item.product_code].discount(item)
   end
 
   def empty?
